a6e05f099ad57a1da4f29129ae6fee0fed05ae48
akinori0518 <aki.yamamoto0518@gmail.com>
2020-06-10 09:02:47 +0900

    lesson10-5

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,44 @@
 $(function(){
-  
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message-list-box">
+          <div class="message-list-box__user-info">
+            <div class="user-info__name">
+              ${message.user_name}
+            </div>
+            <div class="user-info__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-list-box__message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message-list-box">
+          <div class="message-list-box__user-info">
+            <div class="user-info__name">
+              ${message.user_name}
+            </div>
+            <div class="user-info__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-list-box__message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,6 +51,11 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+    })
   });
   
 });
+
+
