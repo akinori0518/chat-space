832cb264d4cb1b5f534ae04eaf2b2a6bfbdd6c0f
akinori0518 <aki.yamamoto0518@gmail.com>
2020-06-21 17:32:23 +0900

    check8

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,29 +63,33 @@ $(function(){
   });
   });
 
-  var reloadMessages = function() {
+  $(function() {
+
+    var reloadMessages = function() {
     
-    var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
       });
-      $('.main-chat__message-list').append(insertHTML);
-      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
+    };
+  })
+  
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
